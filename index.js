f80f08813deb913603b86ea2bf70801cdcf58fa9
Lea Ellingwood <leaellingwood@hotmail.com>
2018-11-05 15:57:15 -0500

    first commit; need work on final error

---
+++ index.js
@@ -0,0 +1,50 @@
+const { Client } = require ('pg');
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+const client = new Client ({
+    database: 'social-media'
+});
+
+
+app.get('/users', (req, res) => {
+    client.query('SELECT * FROM users', (err, result) => {
+        res.send(result.rows);
+        if (err){
+            console.log(err);
+            res.status(500);
+            return res.send(err, "things went bad, mate.")
+        }
+    });
+});
+
+app.post('/users', (req, res) => {
+    
+    const text = 'INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *';
+    const values = [req.body.username, req.body.bio]
+    
+    client.query(text, values, (err, result) => {
+        console.log(result.rows[0]);
+        res.status(201);
+        res.send(result.rows[0])
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id
+    const values = [id]
+    client.query('SELECT * FROM users WHERE id=$1', values, (err, result) => {
+        res.send(result.rows[0]);
+        if (err){
+            console.log(err);
+            res.status(500);
+            return res.send(err, "things went bad, mate.")
+        }
+    });
+});
+
+
+app.listen(3000, () => {
+    client.connect();
+});
